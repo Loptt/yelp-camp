e2a5099d71352fd4a6ef4375d5600157da83e5cc
Carlos Estrada <carlos.deg02@hotmail.com>
2018-12-20 09:27:14 -0600

    Corrected port

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("YelpCamp server has started!");
 });
